364dde90c48d11ce399b1c337c40c422b9bd6627
amador toledo <amadortoledo5@gmail.com>
2024-05-06 20:07:50 -0300

    todavia falta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { AuthComponent } from './layout/auth/auth.component';
 const routes: Routes = [{ path: 'dashboard',
 component: DashboardComponent,
 loadChildren: () =>
@@ -9,6 +10,7 @@ loadChildren: () =>
   ),
 },
 {path:'auth' ,
+component:AuthComponent,
 loadChildren: () => import('./layout/auth/auth.module').then((m) => m.AuthModule)
 },
 {
--- src/app/layout/auth/auth.module.ts
+++ src/app/layout/auth/auth.module.ts
@@ -1,14 +1,20 @@
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { AuthComponent } from './auth.component';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatCardModule,
+    LoginModule
   ]
 })
 export class AuthModule { }
